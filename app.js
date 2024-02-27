04b5017189ee0f76e39b23cf2a4a3017e6902032
salman saghir <salman.saghir1@gmail.com>
2024-02-27 09:15:44 +0500

    questions

---
+++ app.js
@@ -0,0 +1,63 @@
+//.............Chapter-01............
+//Q1:
+//alert("Hello World");
+//Q2:
+//alert("Error! Please enter a valid password.");
+//Q3:
+//alert("Welcome to JS land..\n Happy Coding!");
+//Q4:
+//alert("Welcome to JS land..");
+//alert("Happy Coding!");
+//Q5:
+//console.log("Hello... i can run JS through my web browser's console");
+
+//........Chapter-02..........
+//Q1:
+//var username;
+//Q2:
+//var myName="Salman Saghir";
+//Q3:
+//var message;
+//message="Hello World";
+//alert(message);
+//Q4:
+//var studentName="Salman Sahgir";
+//var age="35 years old";
+//var course="Mobile Web and App Development";
+//alert(studentName);
+//alert(age);
+//alert(course);
+//Q5:
+//var abc="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(abc);
+//Q6:
+//var email="My Eamil Address is salman.saghir1@gmail.com";
+//alert(email);
+//Q7:
+//var book="A smarter way to learn JavaScript";
+//alert(book);
+//Q8:
+//var book="Yah! I can write HTML content through JavaScript";
+//document.write(book);
+//Q9:
+//var abc="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(abc);
+
+//..........Chapter-03...........
+//Q1:
+//var age="I am 35 years old";
+//alert(age);
+//Q2:
+//var N=14;
+//var abc= "You have visited this site " +N+ " times";
+//alert(abc);
+//Q3:
+//var birthYear="1988";
+//document.write("My birth year is", birthYear);
+//Q4:
+//var visitorName="John Doe";
+//var productTitle="T-shirt";
+//var qty="5";
+//document.write(visitorName+" Ordered" +qty+productTitle+"(S) on XYZ Clothing Store.");
+
+
